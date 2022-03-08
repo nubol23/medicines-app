1f21c3434ddd7618b0b5442e43c518d11c2be7ea
nubol23 <rafael.rvp98@gmail.com>
2022-03-08 17:37:55 -0400

    created test configs

---
+++ config/settings/test.py
@@ -0,0 +1,30 @@
+import os
+from pathlib import Path
+
+import dotenv
+
+# Setup test environment variables
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+dotenv.load_dotenv(f"{BASE_DIR}/.env")
+
+os.environ["DJANGO_DEBUG"] = "True"
+os.environ["DJANGO_TESTING"] = "True"
+
+from .default import *  # noqa: E402, F403, F401
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DB_NAME", "config"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASS"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT", 5432),
+        "ATOMIC_REQUESTS": True,
+        "TEST": {"NAME": "test_repositax"},
+    }
+}
+
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+
+DEFAULT_FILE_STORAGE = "inmemorystorage.InMemoryStorage"
