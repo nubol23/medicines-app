5ef52c715a4a58d24a77ffe3c97fc93501a9f167
nubol23 <rafael.rvp98@gmail.com>
2022-03-07 23:38:45 -0400

    added token urls

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from users.views import CustomTokenObtainPairView
+
+app_name = "users"
+
+
+urlpatterns = [
+    path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
