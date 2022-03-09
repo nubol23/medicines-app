2b05bff2ad65d30f5af083ac1abbc5d0de4ddd6e
nubol23 <rafael.rvp98@gmail.com>
2022-03-09 13:20:51 -0400

    added pagination

--- utils/tests/validation.py
+++ utils/tests/validation.py
@@ -1,7 +1,20 @@
+import json
+
+
 class ValidateMultiple:
     @classmethod
-    def validate(cls, testcase, validation_func, objs, dicts):
-        testcase.assertEqual(len(objs), len(dicts))
+    def validate(cls, testcase, validation_func, objs, data):
+        testcase.assertEqual(len(objs), data.pop("count"))
+        dicts = data.pop("results")
+        testcase.assertIsInstance(dicts, list)
 
         for o, d in zip(objs, dicts):
             validation_func(testcase, o, d)
+
+
+def jsonify(content):
+    return json.dumps(content, indent=4)
+
+
+def print_json(content):
+    print(json.dumps(content, indent=4))
