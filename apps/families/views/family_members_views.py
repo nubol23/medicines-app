7a8ac3fedd48f238d43b81289e59575c53e890d7
nubol23 <rafael.rvp98@gmail.com>
2022-03-09 16:31:42 -0400

    added family member viewset

---
+++ apps/families/views/family_members_views.py
@@ -0,0 +1,17 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from apps.families.models import Membership
+from apps.families.serializers import FamilyMemberSerializer
+
+
+class FamilyMembersViewSet(ModelViewSet):
+    queryset = Membership.objects.all()
+    serializer_class = FamilyMemberSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.filter(family__id=self.kwargs["family_id"])
+
+        return qs
