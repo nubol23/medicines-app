f3a4adc30e701702cc0b3f3fdb895556d6a5d070
nubol23 <rafael.rvp98@gmail.com>
2022-03-10 16:51:56 -0400

    added family invitation factory

--- apps/families/tests/factories.py
+++ apps/families/tests/factories.py
@@ -1,7 +1,8 @@
 import factory.django
 
-from apps.families.models import Family, Membership
+from apps.families.models import Family, FamilyInvitation, Membership
 from apps.users.tests.factories import UserFactory
+from utils.tests.faker import faker
 
 
 class FamilyFactory(factory.django.DjangoModelFactory):
@@ -17,3 +18,12 @@ class MembershipFactory(factory.django.DjangoModelFactory):
 
     family = factory.SubFactory(FamilyFactory)
     user = factory.SubFactory(UserFactory)
+
+
+class FamilyInvitationFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = FamilyInvitation
+
+    email = factory.LazyAttribute(lambda _: faker.email())
+    family = factory.SubFactory(FamilyFactory)
+    invited_by = factory.SubFactory(UserFactory)
