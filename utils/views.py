5fc853adf6e9216920cb843a45feaf7fc8064458
nubol23 <rafael.rvp98@gmail.com>
2022-03-09 23:40:45 -0400

    refactored test views

--- utils/views.py
+++ utils/views.py
@@ -20,20 +20,22 @@ class CustomModelViewSet(ModelViewSet):
 
         headers = self.get_success_headers(serializer.data)
         # Return that instead of the original
-        return Response(response_serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(
+            response_serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
 
     def update(self, request, *args, **kwargs):
         """
         Change the original implementation to return a different serializer
         after updating the instance
         """
-        partial = kwargs.pop('partial', False)
+        partial = kwargs.pop("partial", False)
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
 
-        if getattr(instance, '_prefetched_objects_cache', None):
+        if getattr(instance, "_prefetched_objects_cache", None):
             instance._prefetched_objects_cache = {}
 
         # Get default serializer class as response
