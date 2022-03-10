04e6cda12e81005cb26ac6b01f3feb377596b9f4
nubol23 <rafael.rvp98@gmail.com>
2022-03-10 16:48:09 -0400

    added inviter

--- apps/families/models/invitations.py
+++ apps/families/models/invitations.py
@@ -2,6 +2,7 @@ from django.contrib.postgres.fields import CIEmailField
 from django.db import models
 
 from apps.families.models import Family
+from apps.users.models import User
 from utils.models import BaseModel
 
 
@@ -14,10 +15,13 @@ class InvitationStatus(models.TextChoices):
 class FamilyInvitation(BaseModel):
     Status = InvitationStatus
 
-    user_email = CIEmailField(max_length=255)
+    email = CIEmailField(max_length=255)
     family = models.ForeignKey(
         Family, related_name="invitations", on_delete=models.PROTECT
     )
     status = models.CharField(
         max_length=2, choices=Status.choices, default=Status.PENDING
+    )
+    invited_by = models.ForeignKey(
+        User, related_name="created_invitations", on_delete=models.PROTECT
     )
