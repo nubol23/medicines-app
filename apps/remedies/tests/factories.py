4f9f733357c8cfad35bf0939923069f1fe8d4a9f
nubol23 <rafael.rvp98@gmail.com>
2022-03-09 22:02:14 -0400

    created purchase factory

--- apps/remedies/tests/factories.py
+++ apps/remedies/tests/factories.py
@@ -1,6 +1,11 @@
-import factory.django
+from datetime import datetime
 
-from apps.remedies.models import Medicine
+import factory.django
+from django.utils import timezone
+
+from apps.families.tests.factories import FamilyFactory
+from apps.remedies.models import Medicine, Purchase
+from apps.users.tests.factories import UserFactory
 from utils.tests.faker import faker
 
 
@@ -12,3 +17,16 @@ class MedicineFactory(factory.django.DjangoModelFactory):
     maker = factory.Sequence(lambda n: f"Maker {n}")
     quantity = factory.LazyAttribute(lambda _: faker.pyfloat(positive=True))
     unit = factory.LazyAttribute(lambda _: faker.lexify("??"))
+
+
+class PurchaseFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Purchase
+
+    medicine = factory.SubFactory(MedicineFactory)
+    user = factory.SubFactory(UserFactory)
+    family = factory.SubFactory(FamilyFactory)
+    buy_date = factory.LazyAttribute(lambda _: datetime.now(tz=timezone.utc))
+    expiration_date = factory.LazyAttribute(
+        lambda _: faker.future_datetime(tzinfo=timezone.utc)
+    )
