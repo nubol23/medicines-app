e6cb56b06f7bd15df36d60799e442aa2cee559e6
nubol23 <rafael.rvp98@gmail.com>
2022-03-17 15:54:10 -0400

    added status to member serializer

--- apps/families/tests/validators/member_validators.py
+++ apps/families/tests/validators/member_validators.py
@@ -1,9 +1,19 @@
+from apps.families.models import FamilyInvitation, InvitationStatus
 from utils.tests.validation import BaseValidator
 
 
 class ValidateMember(BaseValidator):
     @staticmethod
     def _perform_validation(testcase, obj, dict_entry):
+        def _get_status(obj):
+            email = obj.user.email
+            invitation = FamilyInvitation.objects.filter(email=email).first()
+            if invitation and invitation.status == InvitationStatus.PENDING:
+                return "pending"
+            else:
+                return ""
+
+        testcase.assertEqual(_get_status(obj), dict_entry.pop("status"))
         testcase.assertEqual(obj.user.first_name, dict_entry.pop("first_name"))
         testcase.assertEqual(obj.user.last_name, dict_entry.pop("last_name"))
         testcase.assertEqual(obj.user.phone_number, dict_entry.pop("phone_number"))
