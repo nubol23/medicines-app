0636118957957e94738cbe4e2febc33bcd0af23e
nubol23 <rafael.rvp98@gmail.com>
2022-03-17 16:59:43 -0400

    created endpoint with tests

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from apps.users.views import CustomTokenObtainPairView
+from apps.users.views import CustomTokenObtainPairView, UserExistsViewSet
 
 app_name = "users"
 
@@ -9,4 +9,5 @@ app_name = "users"
 urlpatterns = [
     path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("<user_email>/exists/", UserExistsViewSet.as_view(), name="user-exists"),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,7 +1,30 @@
+from drf_spectacular.utils import extend_schema, inline_serializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.serializers import CharField
+from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from apps.users import serializers
+from apps.users.models import User
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = serializers.CustomTokenObtainPairSerializer
+
+
+class UserExistsViewSet(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(
+        summary="Check if user with email exists",
+        description="Return `true` if user with requested email exists, else return `false`.",
+        responses=inline_serializer(
+            "ExistResponse",
+            fields={"exists": CharField(default="true")},
+        ),
+    )
+    def get(self, request=None, format=None, **kwargs):
+        return Response(
+            {"exists": User.objects.filter(email=kwargs["user_email"]).exists()}
+        )
