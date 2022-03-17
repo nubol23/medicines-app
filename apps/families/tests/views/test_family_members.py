cc86b9b14aef6ac8c7c2d40d641a21ad27e0820a
nubol23 <rafael.rvp98@gmail.com>
2022-03-17 16:32:53 -0400

    added tests

--- apps/families/tests/views/test_family_members.py
+++ apps/families/tests/views/test_family_members.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 from rest_framework import status
 
+from apps.families.models import FamilyInvitation, Membership
 from apps.families.tests.factories import (
     FamilyFactory,
     FamilyInvitationFactory,
@@ -62,3 +63,65 @@ class ListFamilyMembersViewSetTests(CustomTestCase):
             self.members + [self.user_membership],
             response.json(),
         )
+
+
+class RemoveFamilyMembersViewSetTests(CustomTestCase):
+    def _build_url(self, family_id, user_id):
+        return reverse(
+            "families:family-members-delete",
+            kwargs={"family_id": family_id, "user_id": user_id},
+        )
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserFactory()
+        cls.family = FamilyFactory()
+        cls.user_membership = MembershipFactory(user=cls.user, family=cls.family)
+        # User has pending invitation
+        FamilyInvitationFactory(email=cls.user.email)
+
+        cls.members = MembershipFactory.create_batch(size=2, family=cls.family)
+
+    def setUp(self):
+        super().setUp()
+        self.backend.login(self.user)
+
+    def test_access_permission(self):
+        url = self._build_url(self.family.id, self.user.id)
+
+        self.backend.logout()
+        self.backend.delete(url, status=status.HTTP_401_UNAUTHORIZED)
+
+        # Member of no family
+        non_member_user = UserFactory()
+        self.backend.login(non_member_user)
+        self.backend.delete(url, status=status.HTTP_403_FORBIDDEN)
+        self.backend.logout()
+
+        # Member of other family
+        MembershipFactory(user=non_member_user)
+        self.backend.login(non_member_user)
+        self.backend.delete(url, status=status.HTTP_403_FORBIDDEN)
+        self.backend.logout()
+
+        self.backend.login(self.user)
+        self.backend.delete(url, status=status.HTTP_200_OK)
+
+    def test_remove_already_member_user(self):
+        user = self.members[0].user
+        url = self._build_url(self.family.id, user.id)
+        self.backend.delete(url, status=status.HTTP_204_NO_CONTENT)
+
+        self.assertFalse(
+            Membership.objects.filter(family=self.family, user=user).exists()
+        )
+
+    def test_remove_pending_member_user(self):
+        url = self._build_url(self.family.id, self.user.id)
+        self.backend.delete(url, status=status.HTTP_204_NO_CONTENT)
+
+        self.assertFalse(
+            Membership.objects.filter(family=self.family, user=self.user).exists()
+        )
+        invitation = FamilyInvitation.objects.get(email=self.user.email)
+        self.assertEqual(invitation.status, "RE")
