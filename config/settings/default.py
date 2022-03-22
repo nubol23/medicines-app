8f0e803cf638f676ab4ab3ac2d869f2123d4232a
nubol23 <rafael.rvp98@gmail.com>
2022-03-22 09:48:21 -0400

    added template and services folders

--- config/settings/default.py
+++ config/settings/default.py
@@ -64,7 +64,10 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "apps", "families", "templates")],
+        "DIRS": [
+            os.path.join(BASE_DIR, "apps", "families", "templates"),
+            os.path.join(BASE_DIR, "apps", "users", "templates"),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
