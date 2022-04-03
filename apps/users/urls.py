ac9a236ac12aaf404e228d1556fe55b0eb4fa547
nubol23 <rafael.rvp98@gmail.com>
2022-04-03 16:24:26 -0400

    added verify token endpoint

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 from apps.users.views import (
     ActivateUserView,
@@ -14,6 +14,7 @@ app_name = "users"
 urlpatterns = [
     path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("<user_email>/exists/", UserExistsViewSet.as_view(), name="user-exists"),
     path("activate", ActivateUserView.as_view(), name="activate-user"),
     path("register", UserViewSet.as_view({"post": "create"}), name="register-user"),
