9dbe5f53c4c9fcd634e99a56bca047f66cf1cdce
nubol23 <rafael.rvp98@gmail.com>
2022-04-09 17:29:44 -0400

    added search by name and maker

--- apps/remedies/views/medicines_views.py
+++ apps/remedies/views/medicines_views.py
@@ -1,3 +1,7 @@
+from functools import reduce
+
+from django.db.models import CharField, Q
+from django.db.models.functions import Lower
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework.permissions import IsAuthenticated
 
@@ -7,6 +11,8 @@ from apps.remedies.serializers import (
     MedicineListRetrieveSerializer,
 )
 from utils.views import CustomModelViewSet
+
+CharField.register_lookup(Lower)
 
 
 @extend_schema_view(
@@ -49,3 +55,22 @@ class MedicinesViewSet(CustomModelViewSet):
         if self.action in ["create", "partial_update"]:
             return self.create_update_serializer_class
         return self.serializer_class
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        q_objects = []
+        name = self.request.query_params.get("name")
+        if name:
+            q_objects.append(Q(name__unaccent__lower__trigram_similar=name))
+            q_objects.append(Q(name__icontains=name))
+
+        maker = self.request.query_params.get("maker")
+        if maker:
+            q_objects.append(Q(maker__unaccent__lower__trigram_similar=maker))
+            q_objects.append(Q(maker__icontains=maker))
+
+        if len(q_objects) > 0:
+            qs = qs.filter(reduce(lambda q_1, q_2: q_1 | q_2, q_objects))
+
+        return qs
