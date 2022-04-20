c5199e4a6680adbb3cfed8fac3456b9a0e05e1ba
nubol23 <rafael.rvp98@gmail.com>
2022-04-20 11:15:33 -0400

    changed entrypoint to use gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@ echo "Applying migrations"
 ./manage.py migrate
 
 echo "Starting server"
-./manage.py runserver 0.0.0.0:"$PORT"
+#./manage.py runserver 0.0.0.0:"$PORT"
+gunicorn config.wsgi:application --bind 0.0.0.0:"$PORT"
