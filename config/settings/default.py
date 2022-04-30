b4803cf8851d1f57c27e3c85b907e3ee58e48d0a
nubol23 <rafael.rvp98@gmail.com>
2022-04-30 10:36:01 -0400

    fixed page size

--- config/settings/default.py
+++ config/settings/default.py
@@ -159,7 +159,7 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_PAGINATION_CLASS": "utils.pagination.CustomPageNumber",
-    "PAGE_SIZE": 1,
+    "PAGE_SIZE": 10,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
